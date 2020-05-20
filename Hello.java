c9cca9c894855c5eb6615ad618976abd0942dffe
yeshwanth2812 <yesh281297@gmail.com>
2020-05-20 15:19:39 +0530

    hello pgm

--- Hello.java
+++ Hello.java
@@ -1,12 +1,9 @@
-class Hello
+class Hello 
 {
  public static void main(String[] args)
   {
     System.out.println("hello java world ");
-<<<<<<< HEAD
     System.out.println("local Changes");
-=======
     System.out.println("remote changes");
->>>>>>> 025b25357cadf69f1236c845dd0fd4b4877bbff6
    }
 }
